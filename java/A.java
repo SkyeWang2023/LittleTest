9f35bfb6838805e4c94581af182de1aeffc2d650
caiyunw <wangcaiyun198600@163.com>
2026-04-08 13:50:53 +0800

    remove files

--- java/A.java
+++
@@ -1,9 +0,0 @@
-public class A {
-    private B b = new B();
-
-    public int execute(int x) {
-        int result = b.process(x);
-        // 逻辑依赖 B 的结果
-        return result * 3;
-    }
-}
--- java/C.java
+++
@@ -1,6 +0,0 @@
-public class C {
-    public int calculate(int x) {
-        // 原始逻辑：乘以 2
-        return x * 2;
-    }
-}
