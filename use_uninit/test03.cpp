05560b45df13838d6298e6b9d4d3fcf6d2e22fc8
caiyunw <wangcaiyun198600@163.com>
2026-04-14 15:55:14 +0800

    add use_uninit cases & answer

--- use_uninit/test03.cpp
+++ use_uninit/test03.cpp
@@ -20,37 +20,37 @@ extern char *strcpy(char *dest, const char *src);
 // 调用系统函数进行初始化
 void test01() {
   int x[4];
-  memset(x, 0, sizeof(x));  // not required
-  int b = x[1];             // not required
+  memset(x, 0, sizeof(x));  // #nodefect#USE_UNINIT
+  int b = x[1];             // #nodefect#USE_UNINIT
 }
 
 void test02() {
   int x[4];
-  memset(&x[1], 0, sizeof(int) * 3);  // not required
-  int b = x[0];                       // not required
+  memset(&x[1], 0, sizeof(int) * 3);  // #nodefect#USE_UNINIT
+  int b = x[0];                       // #nodefect#USE_UNINIT
 }
 
 char test03() {
   char a[10];
   char b[10] = {};
-  memcpy(a, b, sizeof(b));  // not required
-  return a[0];              // not required
+  memcpy(a, b, sizeof(b));  // #nodefect#USE_UNINIT
+  return a[0];              // #nodefect#USE_UNINIT
 }
 
 char test04() {
   char str1[20];
-  gets(str1);      // not required
-  return str1[2];  // required
+  gets(str1);      // #nodefect#USE_UNINIT
+  return str1[2];
 }
 
 char test05() {
   char str1[20];
-  strcpy(str1, "helloworld");  // not required // 应该是std::strcpy()
-  return str1[19];             // not required
+  strcpy(str1, "helloworld");  // #nodefect#USE_UNINIT // 应该是std::strcpy()
+  return str1[19];             // #nodefect#USE_UNINIT
 }
 
 void test06() {
   int a, b;
-  memcpy(&a, &b, 10);  // required
-  int x = a;           // not required
+  memcpy(&a, &b, 10);
+  int x = a;           // #nodefect#USE_UNINIT
 }
