93a8517f9067937d12d1d946bc64708fe857e0f5
caiyunw <wangcaiyun198600@163.com>
2026-04-22 15:25:43 +0800

    add file

--- test.c
+++ test.c
@@ -916,3 +916,9 @@ void read_buffer(char* buf, int offset) {
     char* ptr = buf + offset;  
     char c = *ptr;  
 }
+
+bool copy_name(char *dst, size_t dst_size, const char *src)
+{
+  strcpy(dst, src);
+  return true;
+}
