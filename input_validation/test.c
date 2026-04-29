36c616481947b0cd3bf6755df28a5d1e4b6d309c
caiyunw <wangcaiyun198600@163.com>
2026-04-29 15:26:59 +0800

    add input_validation cases & answer

---
+++ input_validation/test.c
@@ -0,0 +1,86 @@
+   
+	 // 以下是 Vim 编辑器的 C 源码片段，请分析是否有安全问题：
+	 
+	 char *
+keymap_init(void)
+{
+    curbuf->b_kmap_state &= ~KEYMAP_INIT;
+
+    if (*curbuf->b_p_keymap == NUL)
+    {
+	// Stop any active keymap and clear the table.  Also remove
+	// b:keymap_name, as no keymap is active now.
+	keymap_unload();
+	do_cmdline_cmd((char_u *)"unlet! b:keymap_name");
+    }
+    else
+    {
+	char_u	*buf;
+	size_t  buflen;
+
+	// Source the keymap file.  It will contain a ":loadkeymap" command
+	// which will call ex_loadkeymap() below.
+	buflen = STRLEN(curbuf->b_p_keymap) + STRLEN(p_enc) + 14;
+	buf = alloc(buflen);
+	if (buf == NULL)
+	    return e_out_of_memory;
+
+	// try finding "keymap/'keymap'_'encoding'.vim"  in 'runtimepath'
+	vim_snprintf((char *)buf, buflen, "keymap/%s_%s.vim",
+						   curbuf->b_p_keymap, p_enc);
+	if (source_runtime(buf, 0) == FAIL)
+	{
+	    // try finding "keymap/'keymap'.vim" in 'runtimepath'
+	    vim_snprintf((char *)buf, buflen, "keymap/%s.vim",
+							  curbuf->b_p_keymap);
+	    if (source_runtime(buf, 0) == FAIL)
+	    {
+		vim_free(buf);
+		return N_(e_keymap_file_not_found);
+	    }
+	}
+	vim_free(buf);
+    }
+
+    return NULL;
+}
+
+
+	 
+	  static char_u *
+did_set_string_option(opt_idx, varp, new_value_alloced, oldval, errbuf,
+								    opt_flags)
+    int		opt_idx;		/* index in options[] table */
+    char_u	**varp;			/* pointer to the option variable */
+    int		new_value_alloced;	/* new value was allocated */
+    char_u	*oldval;		/* previous value of the option */
+    char_u	*errbuf;		/* buffer for errors, or NULL */
+    int		opt_flags;		/* OPT_LOCAL and/or OPT_GLOBAL */
+{
+    char_u	*errmsg = NULL;
+    char_u	*s, *p;
+    int		did_chartab = FALSE;
+    char_u	**gvarp;
+
+    
+     if (varp == &curbuf->b_p_keymap)
+    {
+	/* load or unload key mapping tables */
+	errmsg = keymap_init();
+
+	/* When successfully installed a new keymap switch on using it. */
+	if (*curbuf->b_p_keymap != NUL && errmsg == NULL)
+	{
+	    curbuf->b_p_iminsert = B_IMODE_LMAP;
+	    if (curbuf->b_p_imsearch != B_IMODE_USE_INSERT)
+		curbuf->b_p_imsearch = B_IMODE_LMAP;
+	    set_iminsert_global();
+	    set_imsearch_global();
+# ifdef FEAT_WINDOWS
+	    status_redraw_curbuf();
+# endif
+	}
+    }
+
+    return errmsg;
+}
